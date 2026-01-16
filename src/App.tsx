a3bf7e2f71ed273b78bb63dcbd6222f6a8eafac1
surafel <surafelmengist2015@gmail.com>
2026-01-16 19:21:06 +0300

    fundamentals of react

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,9 @@
-import Surafel from "./reactcore/Surafel"
-import AdvanceProp from "./AdvanceProp"
-import Counter from "./reactcore/Counter"
-import { counterProp } from "./reactcore/Counter"
-const App = (props:counterProp) => {
-   const [count,setCount] = useState<number>(0);
-    const handleInc =()=>{
-        setCount(prev=>prev+1)
-    }
+import Object from "./reactcore/Object"
+
+const App = () => {
   return (
     <div>
-      <Surafel 
-      name="surafel"  
-      age={23}
-      email="surafel@gmail.com"
-    
-      pro={ <AdvanceProp
-          id={1}
-           name="abel"
-           department="computer science"
-        />}
-        count={
-          <Counter />
-        } 
-      />
-     
+       <Object />
     </div>
   )
 }
