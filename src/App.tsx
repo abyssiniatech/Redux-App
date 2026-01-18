db205866bec812f53a1907fa5cc215e0c779429b
surafel <surafelmengist2015@gmail.com>
2026-01-18 18:22:25 +0300

    day 44 and deep in dive redux

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(){
 
  return(
   <div className="bg-indigo-600 h-screen flex justify-center items-center">
-     <p>{count}</p>
+     <p className="text-white">{count}</p>
 
      <button className="bg-teal-600 text-white px-4 py-2 rounded m-2 text-2xl" onClick={()=>dispatch(increment())}>Increment</button>
      <button className="bg-teal-600 text-white px-4 py-2 rounded m-2 text-2xl" onClick={()=>dispatch(decrement())}>Decrement</button>
